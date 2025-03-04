1c3cf465244875f97aeb61e01b7c38fc0435613c
Sunny Gilchrist <2983018g@student.gla.ac.uk>
2025-03-04 23:45:04 +0000

    add about page and hyperlinks between pages

--- rango/views.py
+++ rango/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Welcome to Rango!")
+    return HttpResponse("Welcome to Rango! <a href=\"/rango/about/\">Find out more about Rango here.</a>")
+
+def about(request):
+    return HttpResponse("Welcome to Rango's about page! <a href=\"/rango/\">Go home.</a>")
