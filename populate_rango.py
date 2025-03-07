646e648622f25eb0efe6bed47cdded2d8be081fa
Sunny Gilchrist <2983018g@student.gla.ac.uk>
2025-03-07 15:12:45 +0000

    complete chapter 5 exercises

--- populate_rango.py
+++ populate_rango.py
@@ -1,4 +1,7 @@
 import os
+
+from rango import views
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE',
                       'tango_with_django_project.settings')
 
@@ -10,11 +13,12 @@ def populate():
 
     python_pages = [
         {'title': 'Official Python Tutorial',
-         'url':'http://docs.python.org/3/tutorial/'},
+         'url':'http://docs.python.org/3/tutorial/',},
         {'title':'How to Think like a Computer Scientist',
          'url':'http://www.greenteapress.com/thinkpython/'},
         {'title':'Learn Python in 10 Minutes',
-         'url':'http://www.korokithakis.net/tutorials/python/'} ]
+         'url':'http://www.korokithakis.net/tutorials/python/'},
+    ]
 
     django_pages = [
         {'title':'Official Django Tutorial',
@@ -30,12 +34,12 @@ def populate():
         {'title':'Flask',
          'url':'http://flask.pocoo.org/'} ]
 
-    cats = {'Python': {'pages': python_pages,},
-            'Django': {'pages': django_pages,},
-            'Other Frameworks': {'pages': other_pages,} }
+    cats = {'Python': {'pages': python_pages, 'views': 128, 'likes': 64},
+            'Django': {'pages': django_pages, 'views': 64, 'likes': 32},
+            'Other Frameworks': {'pages': other_pages, 'views': 32, 'likes': 16} }
 
     for cat, cat_data in cats.items():
-        c = add_cat(cat)
+        c = add_cat(cat, cat_data['views'], cat_data['likes'])
         for p in cat_data['pages']:
             add_page(c, p['title'], p['url'])
 
@@ -50,8 +54,10 @@ def add_page(cat, title, url, views=0):
     p.save()
     return p
 
-def add_cat(name):
+def add_cat(name, views=0, likes=0):
     c = Category.objects.get_or_create(name=name)[0]
+    c.views=views
+    c.likes=likes
     c.save()
     return c
 
